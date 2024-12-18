d770c3fd8966b869d5bb6a21ad81db06b6c1652f
BudiNugroho256 <budinugraha177@gmail.com>
2024-12-18 11:17:35 +0700

    Update about-us.blade.php

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
